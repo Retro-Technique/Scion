1e48c11ae574df86b8dd5edb736ead850d7f4fd2
Olivier Choquet <olivier.choquet@aloatec.com>
2024-08-06 08:40:37 +0200

    Scion-Audio

---
+++ VisualC/Scion-Audio/MMIOFile.cpp
@@ -0,0 +1,366 @@
+/**
+ *
+ * Scion
+ *
+ * CEA CNRS INRIA LOGICIEL LIBRE
+ *
+ * Copyright(c) 2014-2024 Retro Technique
+ *
+ * This software is a computer program whose purpose is to provide
+ * a 2D game engine.
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ *
+ */
+
+#include "pch.h"
+#include "MMIOFile.h"
+
+namespace scion
+{
+	namespace sfx
+	{
+		namespace priv
+		{
+
+			struct TMMIOFile
+			{
+				HMMIO			_hMMIO;
+				WAVEFORMATEX*	_pWaveFormat;
+				MMCKINFO		_ckIn;
+				MMCKINFO		_ckInRIFF;
+			};
+
+		}
+
+#pragma region Constructors
+
+		IMPLEMENT_DYNAMIC(CMMIOFile, CObject)
+
+		CMMIOFile::CMMIOFile()
+			: m_pImpl(NULL)
+			, m_pData(NULL)
+		{
+			m_pImpl = new priv::TMMIOFile{ 0 };
+		}
+
+		CMMIOFile::~CMMIOFile()
+		{
+			Unload();
+			delete m_pImpl;
+		}
+
+#pragma endregion
+#pragma region Operations
+
+		HRESULT CMMIOFile::LoadFromFile(LPCTSTR pszFileName)
+		{
+			ASSERT_VALID(this);
+
+			if (!AfxIsValidString(pszFileName, MAX_PATH))
+			{
+				return E_INVALIDARG;
+			}
+			
+			HRESULT hr = S_OK;
+
+			do
+			{
+				hr = Open(pszFileName);
+				if (FAILED(hr))
+				{
+					break;
+				}
+
+				hr = StartRead();
+				if (FAILED(hr))
+				{
+					break;
+				}
+
+				m_pData = reinterpret_cast<LPBYTE>(VirtualAlloc(NULL, m_pImpl->_ckIn.cksize, MEM_COMMIT, PAGE_READWRITE));
+				if (!m_pData)
+				{
+					hr = E_OUTOFMEMORY;
+					break;
+				}
+
+				WORD uActualRead = 0;
+				hr = Read(m_pImpl->_ckIn.cksize, m_pData, &uActualRead);
+
+			} while (FALSE);
+
+			return hr;
+		}
+
+		void CMMIOFile::Unload()
+		{
+			if (m_pData)
+			{
+				VirtualFree(m_pData, dzd, MEM_DECOMMIT);
+				m_pData = NULL;
+			}
+
+			Close();
+		}
+
+#pragma endregion
+#pragma region Overridables
+
+#if defined(_DEBUG) || defined(_AFXDLL)
+
+		void CMMIOFile::AssertValid() const
+		{
+			CObject::AssertValid();
+
+			ASSERT_POINTER(m_pImpl, priv::TMMIOFile);
+		}
+
+		void CMMIOFile::Dump(CDumpContext& dc) const
+		{
+			CObject::Dump(dc);
+		}
+
+#endif
+
+#pragma endregion
+#pragma region Implementations
+
+		HRESULT CMMIOFile::Open(LPCTSTR pszFileName)
+		{
+			ASSERT_VALID(this);
+			ASSERT(AfxIsValidString(pszFileName, MAX_PATH));
+
+			HRESULT hr = S_OK;
+
+			do
+			{
+				m_pImpl->_hMMIO = mmioOpen(const_cast<LPTSTR>(pszFileName), NULL, MMIO_ALLOCBUF | MMIO_READWRITE);
+				if (!m_pImpl->_hMMIO)
+				{
+					MMIOERR_ACCESSDENIED;
+					MMIOERR_INVALIDFILE;
+					MMIOERR_NETWORKERROR;
+					MMIOERR_PATHNOTFOUND;
+					MMIOERR_SHARINGVIOLATION;
+					MMIOERR_TOOMANYOPENFILES;
+					hr = E_FAIL;
+					break;
+				}
+				
+				MMRESULT mmRes = mmioDescend(m_pImpl->_hMMIO, &m_pImpl->_ckInRIFF, NULL, 0);
+				if (MMSYSERR_NOERROR != mmRes)
+				{
+					MMIOERR_CHUNKNOTFOUND;
+					hr = E_FAIL;
+					break;
+				}
+
+				if ((FOURCC_RIFF != m_pImpl->_ckInRIFF.ckid) || mmioFOURCC('W', 'A', 'V', 'E') != m_pImpl->_ckInRIFF.fccType)
+				{
+					// not a wave file
+					hr = E_FAIL;
+					break;
+				}
+
+				m_pImpl->_ckIn.ckid = mmioFOURCC('f', 'm', 't', ' ');
+				mmRes = mmioDescend(m_pImpl->_hMMIO, &m_pImpl->_ckIn, &m_pImpl->_ckInRIFF, MMIO_FINDCHUNK);
+				if (MMSYSERR_NOERROR != mmRes)
+				{
+					MMIOERR_CHUNKNOTFOUND;
+					hr = E_FAIL;
+					break;
+				}
+
+				if (sizeof(PCMWAVEFORMAT) > m_pImpl->_ckIn.cksize)
+				{
+					// not a wave file
+					hr = E_FAIL;
+					break;
+				}
+
+				PCMWAVEFORMAT pcmWaveFormat;
+				mmRes = mmioRead(m_pImpl->_hMMIO, reinterpret_cast<HPSTR>(&pcmWaveFormat), sizeof(PCMWAVEFORMAT));
+				if (sizeof(PCMWAVEFORMAT) != mmRes)
+				{
+					// cannot read
+					hr = E_FAIL;
+					break;
+				}
+
+				WORD uExtraAlloc = 0;
+				if (pcmWaveFormat.wf.wFormatTag != WAVE_FORMAT_PCM)
+				{
+					mmRes = mmioRead(m_pImpl->_hMMIO, reinterpret_cast<HPSTR>(&uExtraAlloc), sizeof(WORD));
+					if (sizeof(WORD) != mmRes)
+					{
+						// cannot read
+						hr = E_FAIL;
+						break;
+					}
+				}
+
+				m_pImpl->_pWaveFormat = reinterpret_cast<WAVEFORMATEX*>(VirtualAlloc(NULL, uExtraAlloc + sizeof(WAVEFORMATEX), MEM_COMMIT, PAGE_READWRITE));
+				if (!m_pImpl->_pWaveFormat)
+				{
+					hr = E_OUTOFMEMORY;
+					break;
+				}
+
+				CopyMemory(m_pImpl->_pWaveFormat, &pcmWaveFormat, sizeof(PCMWAVEFORMAT));
+				if (0 != uExtraAlloc)
+				{
+					mmRes = mmioRead(m_pImpl->_hMMIO, reinterpret_cast<HPSTR>(&m_pImpl->_pWaveFormat->cbSize) + sizeof(WORD), uExtraAlloc);
+					if (uExtraAlloc != mmRes)
+					{
+						// not a wave file
+						hr = E_FAIL;
+						break;
+					}
+				}
+
+				mmRes = mmioAscend(m_pImpl->_hMMIO, &m_pImpl->_ckIn, 0);
+				if (MMSYSERR_NOERROR != mmRes)
+				{
+					MMIOERR_CANNOTSEEK;
+					MMIOERR_CANNOTWRITE;
+					hr = E_FAIL;
+					break;
+				}
+
+			} while (FALSE);
+
+			if (FAILED(hr))
+			{
+				Close();
+			}
+
+			return hr;
+		}
+
+		HRESULT CMMIOFile::StartRead()
+		{
+			ASSERT_VALID(this);
+
+			LONG nRet = mmioSeek(m_pImpl->_hMMIO, m_pImpl->_ckInRIFF.dwDataOffset + sizeof(FOURCC), SEEK_SET);
+			if (-1 == nRet)
+			{
+				// fail seek
+				return E_FAIL;
+			}
+
+			m_pImpl->_ckIn.ckid = mmioFOURCC('d', 'a', 't', 'a');
+			MMRESULT mmRes = mmioDescend(m_pImpl->_hMMIO, &m_pImpl->_ckIn, &m_pImpl->_ckInRIFF, MMIO_FINDCHUNK);
+			if (MMSYSERR_NOERROR != mmRes)
+			{
+				MMIOERR_CHUNKNOTFOUND;
+				return E_FAIL;
+			}
+
+			return S_OK;
+		}
+
+		HRESULT CMMIOFile::Read(DWORD uRead, LPBYTE pData, WORD* pActualRead)
+		{
+			ASSERT_VALID(this);
+
+			MMIOINFO mmioInfoIn = { 0 };
+			MMRESULT mmRes = mmioGetInfo(m_pImpl->_hMMIO, &mmioInfoIn, 0);
+			if (MMSYSERR_NOERROR != mmRes)
+			{
+				// fail get info
+				return E_FAIL;
+			}
+
+			UINT uDataIn = uRead;
+			if (uDataIn > m_pImpl->_ckIn.cksize)
+			{
+				uDataIn = m_pImpl->_ckIn.cksize;
+			}
+
+			m_pImpl->_ckIn.cksize -= uDataIn;
+
+			for (UINT i = 0; i < uDataIn; i++)
+			{
+				if (mmioInfoIn.pchNext == mmioInfoIn.pchEndRead)
+				{
+					mmRes = mmioAdvance(m_pImpl->_hMMIO, &mmioInfoIn, MMIO_READ);
+					if (MMSYSERR_NOERROR != mmRes)
+					{
+						MMIOERR_CANNOTEXPAND;
+						MMIOERR_CANNOTREAD;
+						MMIOERR_CANNOTWRITE;
+						MMIOERR_OUTOFMEMORY;
+						MMIOERR_UNBUFFERED;
+						return E_FAIL;
+					}
+
+					if (mmioInfoIn.pchNext == mmioInfoIn.pchEndRead)
+					{
+						// corrupt wave file
+						return E_FAIL;
+					}
+				}
+
+				*((LPBYTE)pData + i) = *((LPBYTE)mmioInfoIn.pchNext);
+				(LPBYTE)mmioInfoIn.pchNext++;
+			}
+
+			mmRes = mmioSetInfo(m_pImpl->_hMMIO, &mmioInfoIn, 0);
+			if (MMSYSERR_NOERROR != mmRes)
+			{
+				// fail set info
+				return E_FAIL;
+			}
+
+			*pActualRead = uDataIn;
+
+			return S_OK;
+		}
+
+		void CMMIOFile::Close()
+		{
+			ASSERT_VALID(this);
+
+			if (m_pImpl->_pWaveFormat)
+			{
+				const WORD uExtraAlloc = m_pImpl->_pWaveFormat->cbSize;
+				VirtualFree(m_pImpl->_pWaveFormat, uExtraAlloc + sizeof(WAVEFORMATEX), MEM_DECOMMIT);
+				m_pImpl->_pWaveFormat = NULL;
+			}
+
+			if (m_pImpl->_hMMIO)
+			{
+				mmioClose(m_pImpl->_hMMIO, 0);
+				m_pImpl->_hMMIO = NULL;
+			}
+		}
+
+#pragma endregion
+
+	}
+}
---
+++ VisualC/Scion-Audio/dllmain.cpp
@@ -0,0 +1,52 @@
+// dllmain.cpp : Définit les routines d'initialisation pour la DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <afxwin.h>
+#include <afxdllx.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+static AFX_EXTENSION_MODULE ScionAudioDLL = { false, nullptr };
+
+extern "C" int APIENTRY
+DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	// Supprimez cet élément si vous utilisez lpReserved
+	UNREFERENCED_PARAMETER(lpReserved);
+
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		TRACE0("Initialisation de Scion-Audio.DLL !\n");
+
+		// Initialisation unique de la DLL d'extension
+		if (!AfxInitExtensionModule(ScionAudioDLL, hInstance))
+			return 0;
+
+		// Insérez cette DLL dans la chaîne de ressource
+		// REMARQUE : Si cette DLL d'extension est implicitement liée par
+		//  une DLL régulière MFC (par exemple un contrôle ActiveX)
+		//  au lieu d'une application MFC, supprimez
+		//  cette ligne de DllMain et placez-la dans une fonction
+		//  séparée exportée à partir de cette DLL d'extension.  La DLL régulière
+		//  qui utilise cette DLL d'extension doit ensuite explicitement appeler cette
+		//  fonction pour initialiser cette DLL d'extension.  Dans le cas contraire,
+		//  l'objet CDynLinkLibrary ne sera pas attaché à la chaîne de ressource
+		//  de la DLL régulière et de nombreux problèmes
+		//  seront générés.
+
+		new CDynLinkLibrary(ScionAudioDLL);
+
+	}
+	else if (dwReason == DLL_PROCESS_DETACH)
+	{
+		TRACE0("Fin d'exécution de Scion-Audio.DLL !\n");
+
+		// Terminez la bibliothèque avant que les destructeurs soient appelés
+		AfxTermExtensionModule(ScionAudioDLL);
+	}
+	return 1;   // ok
+}
